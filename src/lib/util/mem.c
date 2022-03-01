f3ae5aaa426d3bc0f84e20e774b57a1c20ed0687
Michael Grieco <30534878+michaelg29@users.noreply.github.com>
2022-02-28 23:21:23 -0500

    Re-added function

--- src/lib/util/mem.c
+++ src/lib/util/mem.c
@@ -1,4 +1,8 @@
 #include "mem.h"
+#include "../cmathematics/util/numio.h"
+
+#include <stdlib.h>
+#include <stdio.h>
 
 void conditionalFree(void *val, void (*freeFunc)(void *val))
 {
@@ -6,4 +10,11 @@ void conditionalFree(void *val, void (*freeFunc)(void *val))
     {
         freeFunc(val);
     }
+}
+
+void printHexString(char *array, int n, const char *title)
+{
+    unsigned char *tmp = printByteArr(array, n, 0, 0, 0);
+    printf("%s: %s\n", title, tmp);
+    free(tmp);
 }
--- src/lib/util/mem.h
+++ src/lib/util/mem.h
@@ -2,5 +2,6 @@
 #define MEM_H
 
 void conditionalFree(void *val, void (*freeFunc)(void *val));
+void printHexString(char *array, int n, const char *title);
 
 #endif // MEM_H
