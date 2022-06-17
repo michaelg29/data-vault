7015585094afe91a40011850d7ace9c0a04c29fe
Michael Grieco <30534878+michaelg29@users.noreply.github.com>
2022-06-16 23:03:43 -0400

    Improve test suite

--- test/main.c
+++ test/main.c
@@ -6,43 +6,71 @@
 #include "../src/controller/dv_controller.h"
 
 int DV_DEBUG = 0;
-int retCode = 0;
+
 dv_app app;
-char *buf;
+int retCode = 0;
+char *buf = NULL;
+unsigned int noTests = 0;
+unsigned int noSuccesses = 0;
 
 bool createAccount(const char *pwd)
 {
-    retCode = dv_createAccount(&app, pwd, strlen(pwd));
-    printf("Create account: %d\n", retCode);
-    return retCode == DV_SUCCESS;
+    retCode = dv_createAccount(&app, (unsigned char*)pwd, strlen(pwd));
+    printf("Create account with password %s: %d\n", pwd, retCode);
+    noTests++;
+    bool ret = retCode == DV_SUCCESS;
+    noSuccesses += ret;
+    return retCode;
 }
 
 bool login(const char *pwd)
 {
-    retCode = dv_login(&app, pwd, strlen(pwd));
+    retCode = dv_login(&app, (unsigned char*)pwd, strlen(pwd));
     printf("Login with password %s: %d\n", pwd, retCode);
-    return retCode == DV_SUCCESS;
+    noTests++;
+    bool ret = retCode == DV_SUCCESS;
+    noSuccesses += ret;
+    return retCode;
+}
+
+bool loginFail(const char *pwd)
+{
+    retCode = dv_login(&app, (unsigned char*)pwd, strlen(pwd));
+    printf("Fail login with password %s: %d\n", pwd, retCode);
+    noTests++;
+    bool ret = retCode == DV_INVALID_INPUT;
+    noSuccesses += ret;
+    return retCode;
 }
 
 bool logout()
 {
     retCode = dv_logout(&app);
     printf("Logout: %d\n", retCode);
-    return retCode == DV_SUCCESS;
+    noTests++;
+    bool ret = retCode == DV_SUCCESS;
+    noSuccesses += ret;
+    return retCode;
 }
 
 bool createEntry(const char *entryName)
 {
     retCode = dv_createEntry(&app, entryName);
     printf("Create entry with name %s: %d\n", entryName, retCode);
-    return retCode == DV_SUCCESS;
+    noTests++;
+    bool ret = retCode == DV_SUCCESS;
+    noSuccesses += ret;
+    return retCode;
 }
 
 bool createData(const char *entryName, const char *categoryName, const char *data)
 {
     retCode = dv_createEntryData(&app, entryName, categoryName, data);
     printf("Create (%s) for entry %s under category %s: %d\n", data, entryName, categoryName, retCode);
-    return retCode == DV_SUCCESS;
+    noTests++;
+    bool ret = retCode == DV_SUCCESS;
+    noSuccesses += ret;
+    return retCode;
 }
 
 bool accessData(const char *entryName, const char *categoryName, const char *expected)
@@ -51,7 +79,10 @@ bool accessData(const char *entryName, const char *categoryName, const char *exp
     bool matches = !strcmp((const char*)buf, expected);
     printf("Access %s for entry %s: %s: %d\n", categoryName, entryName, buf, retCode);
     free(buf);
-    return retCode == DV_SUCCESS && matches;
+    noTests++;
+    bool ret = retCode == DV_SUCCESS && matches;
+    noSuccesses += ret;
+    return retCode;
 }
 
 bool accessDataFailure(const char *entryName, const char *categoryName)
@@ -59,21 +90,30 @@ bool accessDataFailure(const char *entryName, const char *categoryName)
     retCode = dv_accessEntryData(&app, entryName, categoryName, &buf);
     printf("Access non-existent %s for entry %s: %d\n", categoryName, entryName, retCode);
     free(buf);
-    return retCode == DV_INVALID_INPUT;
+    noTests++;
+    bool ret = retCode == DV_SUCCESS;
+    noSuccesses += ret;
+    return retCode;
 }
 
 bool deleteData(const char *entryName, const char *categoryName)
 {
     retCode = dv_deleteEntryData(&app, entryName, categoryName);
     printf("Delete %s for entry %s: %d\n", categoryName, entryName);
-    return retCode == DV_SUCCESS;
+    noTests++;
+    bool ret = retCode == DV_SUCCESS;
+    noSuccesses += ret;
+    return retCode;
 }
 
 bool deleteDataFailure(const char *entryName, const char *categoryName)
 {
     retCode = dv_deleteEntryData(&app, entryName, categoryName);
     printf("Delete non-existent %s for entry %s: %d\n", categoryName, entryName);
-    return retCode == DV_INVALID_INPUT;
+    noTests++;
+    bool ret = retCode == DV_SUCCESS;
+    noSuccesses += ret;
+    return retCode;
 }
 
 int main()
@@ -82,7 +122,14 @@ int main()
 
     dv_init(&app);
 
+    createAccount("testPwd");
+    loginFail("test");
+    login("testPwd");
+    logout();
+
     dv_kill(&app);
+
+    printf("%d tests run, %d successes: %.2f%%\n", noTests, noSuccesses, (float)noSuccesses / (float)noTests * 100.0f);
 
     printf("Goodbye, world!\n");
 
