a50d3f86f46a2ca977f6cfc791a8058c91756278
Michael Grieco <30534878+michaelg29@users.noreply.github.com>
2022-07-10 18:27:42 -0400

    Implement preliminary terminal view app

---
+++ src/view/terminal/terminal.h
@@ -0,0 +1,9 @@
+#ifndef TERMINAL_H
+#define TERMINAL_H
+
+#define DV_QUIT -1
+#define DV_INVALID_CMD -2
+
+void run();
+
+#endif // TERMINAL_H
--- test/main.c
+++ test/main.c
@@ -4,9 +4,11 @@
 #include <stdarg.h>
 
 #include "../src/datavault.h"
+#include "../src/view/terminal/terminal.h"
 #include "../src/controller/dv_controller.h"
 
 int DV_DEBUG = 0;
+int DV_TESTMODE = 0;
 
 dv_app app;
 int retCode = 0;
@@ -107,50 +109,57 @@ int main()
 {
     printf("Hello, world!\n");
 
-    const char *GITHUB = "GitHub";
-    const char *GOOGLE = "Google";
-    const char *USERNAME = "Username";
-    const char *PASSWORD = "Password";
-
-    const char *GH_USER = "michaelg29";
-    const char *GH_PWD = "gh_pwd";
-    const char *GG_USER = "michaelgrieco27";
-    const char *GG_PWD = "gg_pwd";
-    const char *GG_PWD2 = "gg_pwd2";
-
-    dv_init(&app);
-
-    createAccount("testPwd");
-    loginFail("test");
-
-    if (login("testPwd"))
+    if (DV_TESTMODE)
     {
-        createEntry(GITHUB);
-        accessDataFailure(GITHUB, PASSWORD);
-        createData(GITHUB, PASSWORD, GH_PWD);
-        accessData(GITHUB, PASSWORD, GH_PWD);
-        createEntry(GOOGLE);
-        accessDataFailure(GOOGLE, PASSWORD);
-        accessData(GITHUB, PASSWORD, GH_PWD);
-        createData(GOOGLE, PASSWORD, GG_PWD);
-        createData(GOOGLE, USERNAME, GG_USER);
-        accessDataFailure(GITHUB, USERNAME);
-        createData(GITHUB, USERNAME, GH_USER);
-        accessData(GOOGLE, PASSWORD, GG_PWD);
-        accessData(GOOGLE, USERNAME, GG_USER);
-        deleteData(GOOGLE, USERNAME);
-        accessDataFailure(GOOGLE, USERNAME);
-        modifyData(GOOGLE, PASSWORD, GG_PWD2);
-        accessData(GOOGLE, PASSWORD, GG_PWD2);
-        modifyData(GOOGLE, USERNAME, GG_USER);
-        accessData(GOOGLE, USERNAME, GG_USER);
+        const char *GITHUB = "GitHub";
+        const char *GOOGLE = "Google";
+        const char *USERNAME = "Username";
+        const char *PASSWORD = "Password";
 
-        logout();
+        const char *GH_USER = "michaelg29";
+        const char *GH_PWD = "gh_pwd";
+        const char *GG_USER = "michaelgrieco27";
+        const char *GG_PWD = "gg_pwd";
+        const char *GG_PWD2 = "gg_pwd2";
+
+        dv_init(&app);
+
+        createAccount("testPwd");
+        loginFail("test");
+
+        if (login("testPwd"))
+        {
+            createEntry(GITHUB);
+            accessDataFailure(GITHUB, PASSWORD);
+            createData(GITHUB, PASSWORD, GH_PWD);
+            accessData(GITHUB, PASSWORD, GH_PWD);
+            createEntry(GOOGLE);
+            accessDataFailure(GOOGLE, PASSWORD);
+            accessData(GITHUB, PASSWORD, GH_PWD);
+            createData(GOOGLE, PASSWORD, GG_PWD);
+            createData(GOOGLE, USERNAME, GG_USER);
+            accessDataFailure(GITHUB, USERNAME);
+            createData(GITHUB, USERNAME, GH_USER);
+            accessData(GOOGLE, PASSWORD, GG_PWD);
+            accessData(GOOGLE, USERNAME, GG_USER);
+            deleteData(GOOGLE, USERNAME);
+            accessDataFailure(GOOGLE, USERNAME);
+            modifyData(GOOGLE, PASSWORD, GG_PWD2);
+            accessData(GOOGLE, PASSWORD, GG_PWD2);
+            modifyData(GOOGLE, USERNAME, GG_USER);
+            accessData(GOOGLE, USERNAME, GG_USER);
+
+            logout();
+        }
+
+        dv_kill(&app);
+
+        printf("%d tests run, %d successes: %.2f%%\n", noTests, noSuccesses, (float)noSuccesses / (float)noTests * 100.0f);
     }
-
-    dv_kill(&app);
-
-    printf("%d tests run, %d successes: %.2f%%\n", noTests, noSuccesses, (float)noSuccesses / (float)noTests * 100.0f);
+    else
+    {
+        run();
+    }
 
     printf("Goodbye, world!\n");
 
